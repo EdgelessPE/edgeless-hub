2d64bfe7d059f2a85ac86066a61ab43ece21dd56
Cno <dsyourshy@qq.com>
2021-02-19 01:07:28 +0800

    做了首页预览框架

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,76 @@
 <template>
-  <a-result title="欢迎，今日无事可做" subTitle="（其实是还没想好放啥">
-    <template #icon>
-      <a-icon type="smile" theme="twoTone" />
-    </template>
-    <template #extra>
-      <a-button v-on:click="openQQLink" type="primary" icon="qq" size="large">
-        加入鹅群
-      </a-button>
-    </template>
-  </a-result>
+<div>
+  <a-page-header
+      title="Cno，下午好"
+      :backIcon="false"
+  >
+  </a-page-header>
+<a-row style="width: 100%">
+  <a-col :span="16">
+    <a-result title="Edgeless Alpha内测计划用户">
+      <template slot="subTitle">
+        <a-tag color="red">
+          Alpha
+        </a-tag>
+        <a-tag color="blue">
+          3.1.0
+        </a-tag>
+      </template>
+      <template #icon>
+        <a-icon type="smile" theme="twoTone" />
+      </template>
+      <template #extra>
+        <a-button>
+          检查Alpha更新
+        </a-button>
+      </template>
+    </a-result>
+  </a-col>
+  <a-col :span="8">
+    <a-card title="有3个插件可更新" :bordered="false" style="width: 300px">
+      <template slot="extra">
+        <a-button type="primary">更新</a-button>
+      </template>
+        <p>随便看看</p>
+        <a-list>
+          <a-button slot="loadMore" type="link" icon="reload" @click="openQQLink">
+            换一批
+          </a-button>
+          <a-list-item>
+            <a-list-item-meta
+                title="PotPlayer"
+                description="影音播放"
+            >
+            </a-list-item-meta>
+            <template slot="extra">
+              <a-button>安装</a-button>
+            </template>
+          </a-list-item>
+          <a-list-item>
+            <a-list-item-meta
+                title="360急救箱"
+                description="安全急救"
+            >
+            </a-list-item-meta>
+            <template slot="extra">
+              <a-button>安装</a-button>
+            </template>
+          </a-list-item>
+          <a-list-item>
+            <a-list-item-meta
+                title="Audition"
+                description="办公编辑"
+            >
+            </a-list-item-meta>
+            <template slot="extra">
+              <a-button>安装</a-button>
+            </template>
+          </a-list-item>
+        </a-list>
+    </a-card>
+  </a-col>
+</a-row>
+</div>
 </template>
 
 <script>
