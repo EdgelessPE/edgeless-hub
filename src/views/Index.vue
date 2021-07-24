95656bc41e2085f8c684538b55825f2c83b46912
Cno <dsyourshy@qq.com>
2021-07-24 15:01:47 +0800

    修复全部插件都需要更新时提示未安装的问题

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -32,7 +32,7 @@
     </a-result>
   </a-col>
   <a-col :span="8">
-    <a-card :title="$store.state.fileList.length===0?'未安装插件':($store.state.updateList.length===0?('已安装'+$store.state.fileList.length+'个插件'):('有'+$store.state.updateList.length+'个插件可更新'))" :bordered="false" style="width: 300px">
+    <a-card :title="$store.state.fileList.length+$store.state.updateList.length===0?'未安装插件':($store.state.updateList.length===0?('已安装'+$store.state.fileList.length+'个插件'):('有'+$store.state.updateList.length+'个插件可更新'))" :bordered="false" style="width: 300px">
       <template slot="extra">
         <a-button v-if="$store.state.updateList.length!==0" type="primary" @click="routeTo('/down')">更新</a-button>
         <a-button v-else @click="routeTo('/down')">管理</a-button>
