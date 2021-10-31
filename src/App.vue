2afc6fa425e2bc5f45fe0b64a9b1bdf623147ccd
董晓天 <34956162+XiaotianDong@users.noreply.github.com>
2021-10-31 09:23:17 +0800

    限定Content 高度为80vh
    要不拉到下面去侧边菜单看不到

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@
           <TopBar/>
         </a-layout-header>
         <a-layout-content
-            :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+            :style="{ margin: '24px 16px', padding: '24px', background: '#fff', overflow: 'auto', height: '80vh'}"
         >
           <router-view/>
         </a-layout-content>
