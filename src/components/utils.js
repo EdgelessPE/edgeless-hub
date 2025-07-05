3823e2a28a28881e5fc3334e5a63791a5a643f72
Cno <dsyourshy@qq.com>
2025-07-05 21:11:03 +0800

    feat: 支持后端获取重定向链接模式

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 
-const regex=/attachment;filename="([^"]+)"/
-const regex2=/attachment; filename\*=UTF\-8''([^"]+)/
+const regex = /attachment;filename="([^"]+)"/
+const regex2 = /attachment; filename\*=UTF\-8''([^"]+)/
 async function fetchURL(url) {
-    console.log('fetchURL',url)
-    return new Promise((resolve,reject) => {
-        axios.get(url, { maxRedirects: 0 }).then((res)=>{
+    console.log('fetchURL', url)
+    return new Promise((resolve, reject) => {
+        axios.get(url, { maxRedirects: 0, headers: { "x-get-redirect": "true" } }).then((res) => {
             // 从 header 里面拿出真实文件名
-            const text=res.headers['content-disposition']
-            console.log('text',text)
-            if(text){
-                const m=text.match(regex)
-                if(m?.length&&m.length>1){
-                     resolve(m[1])
+            const text = res.headers['content-disposition'] ||res.headers['location']
+            console.log('text', text)
+            if (text) {
+                const m = text.match(regex)
+                if (m?.length && m.length > 1) {
+                    resolve(m[1])
                     return
                 }
-                const m2=text.match(regex2)
-                if(m2?.length&&m2.length>1){
+                const m2 = text.match(regex2)
+                if (m2?.length && m2.length > 1) {
                     resolve(m2[1])
                     return
                 }
                 // 如果没有匹配到文件名，就直接返回 URL 的最后一部分
-                const sp=url.split('/')
-                resolve(sp[sp.length-1]);
-            }else{
+                const sp = url.split('/')
+                resolve(sp[sp.length - 1]);
+            } else {
                 reject()
             }
         }).catch((e) => {
@@ -41,14 +41,14 @@ async function fetchURL(url) {
     });
 }
 
-export async function fileNameFetcher(url){
-    const sp=url.split('/')
-    const out=sp[sp.length-1]
-    if(out.includes('.')){
+export async function fileNameFetcher(url) {
+    const sp = url.split('/')
+    const out = sp[sp.length - 1]
+    if (out.includes('.')) {
         return out
-    }else{
-        const real=await fetchURL(url)
-        const sp=real.split('/')
-        return sp[sp.length-1]
+    } else {
+        const real = await fetchURL(url)
+        const sp = real.split('/')
+        return sp[sp.length - 1]
     }
 }
